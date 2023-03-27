21ece541311312ebff5f50ce588f4cd24f9c4dda
Elena <ealvar03@mail.bbk.ac.uk>
2023-03-27 12:03:33 +0100

    Top 10 movies version 1.1

--- main.py
+++ main.py
@@ -7,18 +7,28 @@ from wtforms.validators import DataRequired
 import requests
 
 
-class EditForm(FlaskForm):
-    rating = StringField(label="Your Rating Out of 10", validators=[DataRequired()])
-    review = StringField(label="Your Review", validators=[DataRequired()])
-    submit = SubmitField(label="Done")
-
-
+API_KEY = "Your API Key"
+MOVIES_SEARCH_ENDPOINT = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}"
+MOVIES_ID_ENDPOINT = "https://api.themoviedb.org/3/movie/"
+MOVIES_IMG_URL="https://image.tmdb.org/t/p/w300_and_h450_bestv2"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+class EditForm(FlaskForm):
+    rating = StringField(label="Your Rating Out of 10", validators=[DataRequired()])
+    review = StringField(label="Your Review", validators=[DataRequired()])
+    submit = SubmitField(label="Done")
+
+
+class AddMovie(FlaskForm):
+    title = StringField(label="Movie Title", validators=[DataRequired()])
+    submit = SubmitField(label="Add Movie")
+
 
 # Create table using SQLAlchemy
 with app.app_context():
@@ -27,9 +37,9 @@ with app.app_context():
         title = db.Column(db.String(250), unique=True, nullable=False)
         year = db.Column(db.Integer, nullable=False)
         description = db.Column(db.String(250), nullable=False)
-        rating = db.Column(db.Float, nullable=False)
-        ranking = db.Column(db.Integer, unique=True, nullable=False)
-        review = db.Column(db.String(250), nullable=False)
+        rating = db.Column(db.Float, nullable=True)
+        ranking = db.Column(db.Integer, unique=True, nullable=True)
+        review = db.Column(db.String(250), nullable=True)
         img_url = db.Column(db.String(250), unique=True, nullable=False)
 
         def __repr__(self):
@@ -42,21 +52,50 @@ with app.app_context():
 @app.route("/")
 def home():
     all_movies = db.session.query(Movie).all()
+    for i in range(len(all_movies)):
+        # This line gives each movie a new ranking reversed from their order in all_movies
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
+
     return render_template("index.html", movies=all_movies)
 
 
 # Add new movie to the list
 @app.route('/add', methods=["GET", "POST"])
 def add():
-    if request.method == "POST":
-        data = request.form
-        new_movie = Movie(title=data["title"], year=data["year"], description=data["description"],
-                          rating=data["rating"], ranking=data["ranking"], review=data["review"],
-                          img_url=data["img_url"])
+    form = AddMovie()
+    if form.validate_on_submit():
+        movie_keyword = form.title.data
+        response = requests.get(url=MOVIES_SEARCH_ENDPOINT, params={"api_key": API_KEY, "query": movie_keyword})
+        data = response.json()['results']
+        return render_template('select.html', options=data)
+    return render_template('add.html', form=form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_id = request.args.get('id')
+    if movie_id:
+        movie_id_url = f"{MOVIES_ID_ENDPOINT}{movie_id}?api_key={API_KEY}"
+        response = requests.get(url=movie_id_url, params={"api_key": API_KEY})
+        data = response.json()
+        new_movie = Movie(title=data["title"], year=data["release_date"], description=data["overview"],
+                          img_url=f"{MOVIES_IMG_URL}{data['poster_path']}")
         db.session.add(new_movie)
         db.session.commit()
-        return redirect(url_for('home'))
-    return render_template('add.html')
+        return redirect(url_for('update', id=new_movie.id))
+
+
+@app.route("/select", methods=["GET", "POST"])
+def select():
+    movie_details_endpoint = f"https://api.themoviedb.org/3/movie/603?api_key={API_KEY}"
+    response = requests.get(url=movie_details_endpoint)
+    data = response.json()
+    new_movie = Movie(title=data["title"], year=data["release_date"], description=data["overview"],
+                      rating=None, ranking=None, review=None, img_url=data["poster_path"])
+    db.session.add(new_movie)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 # Update rating and review details using WTForms
